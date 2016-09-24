84a64061a5621b4bb111aedebde0f275449f579f
luis <ferrerluis97@gmail.com>
2016-09-24 01:23:26 -0400

    Implemented basic functionality

--- awesome_options.c
+++ awesome_options.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *args[]) {
 
-    int i;
-    for (i = 1; i < argc; i++) {
-        printf("%s\n", argv[i]);
+    if (argc < 3) {
+        printf("awesome_options needs 2 arguments.\nFirst is a string that represents the arguments passed to another command.\nSecond is a string representing the option whose value we are looking for.");
+        return -1;
     }
 
-    return 0;
+    char* arg = strtok(args[1], " ");
+    int isValue = -1;
+    while(arg != NULL) {
+        if (isValue == 0) {
+            printf("%s\n", arg);
+            return 0;
+        }
+        if (strcmp(arg, args[2]) == 0) {
+            isValue = 0;
+        }
+        arg = strtok(NULL, " ");
+    }
+
+    return -1;
 }
